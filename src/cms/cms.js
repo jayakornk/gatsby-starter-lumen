64096d0e14ba00c229c08faa2f7dccc43d2d1e97
jayakornk <jayakornk@gmail.com>
2019-05-11 00:07:38 +0700

    ✨ added styles for preview 🥓

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,5 +2,8 @@ import CMS from 'netlify-cms';
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
 
+import './assets/fonts/Jost/jost.css';
+import './assets/scss/init.scss';
+
 CMS.registerPreviewTemplate('pages', PagePreview);
 CMS.registerPreviewTemplate('posts', PostPreview);

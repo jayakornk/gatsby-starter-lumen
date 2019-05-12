91d5f8378432ddaf0aaeb1da74f1fb63cd991cba
jayakornk <jayakornk@gmail.com>
2019-05-13 01:27:52 +0700

    🐛 fixed tags bug 🥓

--- content/pages/about.md
+++ content/pages/about.md
@@ -1,7 +1,7 @@
 ---
 template: page
 title: About me
-slug: about
+slug: /pages/about
 draft: false
 ---
 ## Hi, I'm Jayakorn Karikan (Bacon)
--- src/components/Post/Tags/Tags.js
+++ src/components/Post/Tags/Tags.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Tags.module.scss';
 
-const Tags = ({ tags, tagSlugs }) => (
-  <div className={styles['tags']}>
-    <ul className={styles['tags__list']}>
-      {tagSlugs.map((slug, i) => (
-        <li className={styles['tags__list-item']} key={tags[i]}>
-          <Link to={slug} className={styles['tags__list-item-link']}>
-            {tags[i]}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const Tags = ({ tags, tagSlugs }) => {
+  console.log(tagSlugs)
+  return (
+    <div className={styles['tags']}>
+      <ul className={styles['tags__list']}>
+        {tagSlugs ? tagSlugs.map((slug, i) => (
+          <li className={styles['tags__list-item']} key={tags[i]}>
+            <Link to={slug} className={styles['tags__list-item-link']}>
+              {tags[i]}
+            </Link>
+          </li>
+        )) : null}
+      </ul>
+    </div>
+  )
+};
 
 export default Tags;

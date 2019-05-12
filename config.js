529505c9b135fdaf276d1ad347b71697801054b5
jayakornk <jayakornk@gmail.com>
2019-05-12 19:35:32 +0700

    🔧 change URL and author image 🥓

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://goofy-gates-8ff55e.netlify.com',
+  url: 'https://blog.jayakornk.dev',
   title: 'Blog by Jayakorn Karikan',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  copyright: '© 2019 All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -28,11 +28,9 @@ module.exports = {
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: 'jayakornk@gmail.com',
-      telegram: '#',
-      twitter: '#',
       github: 'jayakornk',
+      twitter: '#',
       rss: '#',
-      vkontakte: '#'
     }
   }
 };
